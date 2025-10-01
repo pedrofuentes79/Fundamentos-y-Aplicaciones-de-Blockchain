4224621f8aeef69c1538b3d5148f810856b026cd
Pedro Fuentes <pedrofuentes7799@gmail.com>
2025-10-01 17:11:31 -0300

    Adds front running safety.
    Requires players to send a commitment when they announce they are going to play.
    
    Then, they must reveal their play, sending the secret they used to encode that commitment sent earlier.
    
    Then, we ensure that they are not lying respect to their previous commitment and determine the winner

--- HW2/pennies.sol
+++ HW2/pennies.sol
@@ -8,28 +8,58 @@ contract MatchingPennies {
     address public playerA;
     address public playerB;
     bool public valueA;
+    bool public revealedA;
     bool public valueB;
+    bool public revealedB;
+    bytes32 public commitmentA;
+    bytes32 public commitmentB;
     uint256 public constant REWARD = 0.1 ether;
     mapping(address => uint256) public balances;
 
 
-    function play(bool value) public payable {
+    function commitPlay(bytes32 commitment) public payable {
         require(msg.value == REWARD / 2, "Must pay half the reward to play");
         
         // if A hasn't played yet
         if (playerA == address(0)) {
             playerA = msg.sender;
-            valueA = value;
-            return; // so that we wait until B plays 
+            commitmentA = commitment;
+            return;
         } else if (playerB == address(0)) {
             require(msg.sender != playerA, "Player A cannot play again");
             playerB = msg.sender;
-            valueB = value;
+            commitmentB = commitment;
+            return;
         } else {
             revert("Game already started");
         }
+    }
 
-        // When reaching this point, we know that both have played
+    function revealPlay(bool value, string memory secret) public {
+        require(playerA != address(0) && playerB != address(0), "Both players must have played");
+
+        bytes32 hash = keccak256(abi.encodePacked(value, secret));
+
+        if (msg.sender == playerA) {
+            require(!revealedA, "Player A has already revealed");
+            require(hash == commitmentA, "Commitment does not match");
+            valueA = value;
+        } else if (msg.sender == playerB) {
+            require(!revealedB, "Player B has already revealed");
+            require(hash == commitmentB, "Commitment does not match");
+            valueB = value;
+        } else {
+            revert("Not a player");
+        }
+
+        if (revealedA && revealedB) {
+            determineWinner();
+        }
+    }
+
+    function determineWinner() public {
+        require(revealedA && revealedB, "Both players must have revealed");
+
         address winner;
         if (valueA == valueB){
             winner = playerA;
@@ -40,8 +70,12 @@ contract MatchingPennies {
         // clean up the state
         playerA = address(0);
         playerB = address(0);
+        revealedA = false;
+        revealedB = false;
         valueA = false;
         valueB = false;
+        commitmentA = bytes32(0);
+        commitmentB = bytes32(0);
 
         // have the winner be able to retrieve the funds later.
         balances[winner] += REWARD;
